499a452c87dff6097bce9558cb45dead3f92f949
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2022-02-25 23:18:22 -0500

    Fix syntax error

--- test_sample.py
+++ test_sample.py
@@ -4,4 +4,4 @@ def create_array():
     return np.zeros(shape=(3,3))
 
 def test_create_array():
-    assert np.array_equal(create_array(), np.zeros(shape=(3,3))
+    assert np.array_equal(create_array(), np.zeros(shape=(3,3)))
