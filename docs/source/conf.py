b1811e4123362e515d375c00f47e163f966ab656
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2022-02-26 22:00:16 -0500

    Add flake ignore for unused import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
+import sphinx_rtd_theme  # noqa: F401
 
 sys.path.insert(0, os.path.abspath("."))
 
@@ -67,4 +67,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
