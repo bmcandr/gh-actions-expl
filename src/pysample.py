63fe850da827cf6bd62afc67a1791312099cf878
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2022-04-03 18:17:25 -0400

    Update documentation

--- src/pysample.py
+++ src/pysample.py
@@ -6,9 +6,9 @@ import numpy as np
 
 
 def create_array():
-    """
-    Return a 3x3 np.ndarray of zeroes.
+    """Return a 3x3 np.ndarray of zeroes.
 
+    :return: 3x3 np.ndarray of zeroes
     :rtype: np.ndarray
     """
     return np.zeros(shape=(3, 3))
