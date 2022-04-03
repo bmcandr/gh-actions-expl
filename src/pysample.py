526f6a30d11954cd25c70e6f346dbfa0c3ce29a0
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2022-04-03 18:23:37 -0400

    Add random array func and test

--- src/pysample.py
+++ src/pysample.py
@@ -12,3 +12,21 @@ def create_array():
     :rtype: np.ndarray
     """
     return np.zeros(shape=(3, 3))
+
+
+def create_rand_array(mean: float = 0.0, stdev: float = 1.0, shape: tuple = (3, 3)):
+    """Creates an ndarray with the given shape, mean, and standard deviation.
+
+    :param mean: mean of the array, defaults to 0.0
+    :type mean: float, optional
+    :param stdev: standard deviation of the array, defaults to 1.0
+    :type stdev: float, optional
+    :param shape: shape of the array, defaults to (3, 3)
+    :type shape: tuple, optional
+    :return: ndarray of the given shape with values equal to mean and stdev
+    :rtype: np.ndarray
+    """
+
+    arr = np.zeros(shape=shape)
+
+    return np.random.normal(loc=mean, scale=stdev, size=arr.size).reshape(arr.shape)
